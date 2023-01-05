8472f33aa8fe520008e979195731a0de8be850f6
정동구 <dsaf2007@jeongdong-guui-MacBookAir.local>
2023-01-05 15:34:03 +0900

    2630.cpp

--- DivideAndConquer/2630.cpp
+++ DivideAndConquer/2630.cpp
@@ -7,6 +7,33 @@ int paper[128][128];
 
 int N;
 
+int w, b;
+
+void solution(int x, int y, int n)
+{
+    int color = paper[x][y];
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            if(paper[i][j] != color)
+            {
+                solution(x,y,n);
+                solution(x+ n/2, y, n);
+                solution(x, y+n/2, n);
+                solution(x + n/2, y + n/2, n);
+
+                return;
+            }
+        }
+    }
+    if(color == 1)
+        b ++;
+    else
+        w++;
+
+    return;
+}
 int main()
 {
     std::ios_base::sync_with_stdio(false);
@@ -21,4 +48,8 @@ int main()
             std::cin >> paper[i][j];
         }
     }
+
+    solution(0,0,N);
+
+    std::cout << w << "\n" << b << "\n";
 }
