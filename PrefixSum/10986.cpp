c35d79ced31be2c11902bc496cc3fa728e7a3d05
정동구 <dsaf2007@jeongdong-guui-MacBookAir.local>
2023-01-22 08:12:09 +0900

    10986.cpp

--- PrefixSum/10986.cpp
+++ PrefixSum/10986.cpp
@@ -3,14 +3,13 @@
 
 int n,m;
 long long sum[1000001];
-std::vector<int> mod_sum;
+long long mod[1001];
 
 
 int main()
 {
     std::ios_base::sync_with_stdio(false);
     std::cin.tie(0);
-    std::cout.tie(0);
 
     std::cin >> n >> m;
 
@@ -19,11 +18,14 @@ int main()
         int temp;
         std::cin >> temp;
         sum[i] = sum[i-1] + temp;
-
-        if(sum[i] % m == 0)
-        {
-            mod_sum.push_back(i);
-        }
+        mod[sum[i] % m]++;
     }
+    long long result = 0;
+    for(int i = 0; i < m; i++)
+    {
+        long long temp = mod[i]*(mod[i] - 1)/2;
+        result += temp;
+    }
+    std::cout << result + mod[0] << "\n";
     return 0;
 }
