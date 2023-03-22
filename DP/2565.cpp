9520cac9e3a3a60a5fe77bf9bf239e28d7781673
dsaf2007 <dsaf2007@naver.com>
2023-03-22 20:52:16 +0900

    2565 solved again

--- DP/2565.cpp
+++ DP/2565.cpp
@@ -2,32 +2,26 @@
 #include <vector>
 #include <algorithm>
 
-
 int n;
-int result;
-std::vector<std::pair<int, int>> line;
 int dp[500];
+std::vector<std::pair<int, int>> line;
 
 int main()
 {
     std::ios_base::sync_with_stdio(false);
     std::cin.tie(0);
 
-    std::cin >>n;
-
     for(int i = 0; i < n; i++)
     {
         int a, b;
-
         std::cin >> a >> b;
-
-        line.push_back(std::make_pair(a,b));
+        line.push_back(std::pair<int, int>(a,b));
     }
+
     std::sort(line.begin(), line.end());
 
     for(int i = 0; i < n; i++)
     {
-        dp[i] = 1;
         for(int j = 0; j < i; j++)
         {
             if(line[j].second < line[i].second && dp[i] < dp[j] + 1)
@@ -39,6 +33,7 @@ int main()
 
     int max = *std::max_element(dp, dp + 500);
 
-    std::cout << n -max;
+    std::cout << n - max;
 
+    return 0;
 }
