241e64f103a977ee1d4c1c90c1d1c3716871e804
dsaf2007 <dsaf2007@naver.com>
2023-05-31 01:07:31 +0900

    2579 : 계단오르기 연속하여 세개의 계단을 오를 수 없으므로 해당 경우를 고려하며 bottom-up 으로 dp 배열 완성 마지막 계단은 반드시 밟으므로 마지막 계단의 dp 배열 값을 출력

--- DP/2579.cpp
+++ DP/2579.cpp
@@ -1,30 +1,43 @@
+/*
+목적 : 최대 점수를 받을 수 있는 계단 오르는 방법
+
+조건
+1. 계단은 한 번에 한 계단씩 또는 두 계단씩 오를 수 있다. 즉, 한 계단을 밟으면서 이어서 다음 계단이나, 다음 다음 계단으로 오를 수 있다.
+2. 연속된 세 개의 계단을 모두 밟아서는 안 된다. 단, 시작점은 계단에 포함되지 않는다.
+3. 마지막 도착 계단은 반드시 밟아야 한다.
+계단 개수 300이하
+각 계단별 점수 10000이하
+*/
 #include <iostream>
 #include <algorithm>
 
-int stair[301];
-int dp[301];
-int n;
+using namespace std;
+
+int N;
+int stairs[300]; //계단별 점수
+int dp[300]; // 각 계단까지의 최대값
+
 int main()
 {
-    std::ios_base::sync_with_stdio(false);
-    std::cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
 
-    std::cin >> n;
+    cin >> N;
 
-    for(int i = 0; i < n; i++)
+    for(int i = 0; i < N; i++)
     {
-        std::cin >> stair[i];
+        cin >> stairs[i];
+    }
+    dp[0] = stairs[0];
+    dp[1] = dp[0] + stairs[1];
+    dp[2] = max(dp[0] + stairs[2], stairs[1] + stairs[2]);
+
+    for(int i = 3; i < N; i++)
+    {
+        dp[i] = max(dp[i-3] + stairs[i-1] + stairs[i], dp[i-2] + stairs[i]);
     }
 
-    dp[n-1] = stair[n-1];
-    dp[n-2] = dp[n-1] + stair[n-2];
-    dp[n-3] = std::max(dp[n-1] + stair[n-3], dp[n-2]);
+    cout << dp[N-1];
 
-    for(int i = n - 4; i >=0; i--)
-    {
-        dp[i] = std::max(dp[i+3] + stair[i] + stair[i+1], dp[i+2] + stair[i]);
-    }
-
-    std::cout << dp[0];
     return 0;
 }
