686360e3b74ae3b43fdb551883901699ad155122
dsaf2007 <dsaf2007@naver.com>
2023-07-07 19:22:55 +0900

    순열 조합 정리 재귀를 이용하여 구현한 순열, 중복순열, 조합, 중복조합

--- cpp/practice/combination.cpp
+++ cpp/practice/combination.cpp
@@ -1,47 +1,110 @@
 #include <iostream>
 #include <vector>
 
-void combination(int idx, int k,std::vector<int> arr);
-std::vector<std::vector<int>> combi;
-    std::vector<int> visited(3,0);
-int main()
+using namespace std;
+
+int arr[4];
+int check[4];
+int n = 3;
+int r = 3;
+
+void permutation(int k)
 {
-    
-    std::vector<int> arr(5,0);
-
-    combination(0,0,arr);
-
-    std::cout << combi.size() <<std::endl;
-
-    for(auto x : combi)
+    if(k == r)
     {
-        for(auto i : x)
+        for(int i = 0; i < n; i++)
         {
-            std::cout << i << " ";
-        }
-        std::cout<< "\n";
-    }
-
-    return 0;
-
-}
-
-void combination(int idx, int k,std::vector<int> arr)
-{
-    if(k == 3)
-    {
-        combi.push_back(arr);
+            cout << arr[i] << " ";
+        }cout << "\n";
         return;
     }
-    else{
-        for(int i = idx; i < 5; i++)
-        {
-            if(visited[i] == 1) continue;
 
-            visited[i] = 1;
-            arr[i] =i;
-            combination(i, k+1, arr);
-            visited[i] = 0;
+    for(int i = 1; i <= 3; i++)
+    {
+        if(!check[i])
+        {
+            check[i] = 1;
+            arr[k] = i;
+            permutation(k+1);
+            check[i] = 0;
         }
     }
 }
+
+void permutation_rep(int k)
+{
+    if(k == r)
+    {
+        for(int i = 0; i < n; i++)
+        {
+            cout << arr[i] << " ";
+        }cout << "\n";
+        return;
+    }
+
+    for(int i = 1; i <= 3; i++)
+    {
+        arr[k] = i;
+        permutation_rep(k+1);
+    }
+}
+
+void combination(int k, int idx)
+{
+    if(k == r)
+    {
+        for(int i = 0; i < 2; i++)
+        {
+            cout << arr[i] << " ";
+        }cout << "\n";
+        return;
+    }
+
+    for(int i = idx; i <= n; i++)
+    {
+        arr[k] = i;
+        combination(k + 1, i + 1);
+    }
+}
+
+void combination_rep(int k, int idx)
+{
+    if(k == r)
+    {
+        for(int i = 0; i < 2; i++)
+        {
+            cout << arr[i] << " ";
+        }cout << "\n";
+        return;
+    }
+
+    for(int i = idx; i <= n; i++)
+    {
+        arr[k] = i;
+        combination(k + 1, i);
+    }
+}
+
+
+
+int main()
+{
+    //순열
+    cout << "순열 :\n";
+    permutation(0);
+    //중복순열
+    cout << "===================\n중복순열 :\n";
+    permutation_rep(0);
+
+    n = 4;
+    r = 3;
+    //조합
+    cout << "===================\n조합 :\n";
+    combination(0,1);
+    //중복조합
+    cout << "===================\n중복조합 :\n";
+    combination_rep(0, 1);
+
+    return 0;
+}
+
